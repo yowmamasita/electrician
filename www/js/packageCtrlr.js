f21e0ec1fc1621c7ef0a8185707b2ef86a719664
ken <ken.aque@gmail.com>
2018-11-10 23:52:40 +0800

    display package list

--- www/js/packageCtrlr.js
+++ www/js/packageCtrlr.js
@@ -1,9 +1,20 @@
 angular.module('app.controllers', [])
   
-.controller('packageCtrlr', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
-// You can include any angular dependencies as parameters for this function
-// TIP: Access Route Parameters for your page via $stateParams.parameterName
+.controller('packageCtrlr', ['$scope', '$stateParams',
 function ($scope, $stateParams) {
+	var database = firebase.database();
+	var storage = firebase.storage();
+	var storageRef = firebase.storage().ref();
 
+	return firebase.database().ref('/packages/').once('value').then(function(snapshot) {
+		$scope.records = snapshot.val();
+		angular.forEach($scope.records, function(value, key) {
+        	var imRef = storageRef.child(value.image);
+			imRef.getDownloadURL().then(function(url) {
+	  			value.public_image = url;
+  			});
 
-}])
+		});
+	});
+
+}]);
